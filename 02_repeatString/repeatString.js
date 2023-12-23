5ad2a7a2219fd65213fd7d562d18a1ee7aa757e5
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-23 22:18:00 +0200

    updated repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    repeatedString = "";
 
+    if (times < 0) {
+        return 'ERROR';
+    }
+    for (i=0; i<times; i++) {
+        repeatedString += word;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
