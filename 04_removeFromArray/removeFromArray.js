99d25748d64a517c4cbdb5f2989467da0da8ce4c
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-23 22:52:24 +0200

    updated removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+    newArray = arguments[0];
+    for (i=1; i<arguments.length; i++) {
+        newArray = newArray.filter(x => x !== arguments[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
