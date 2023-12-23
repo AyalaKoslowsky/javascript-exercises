46724d9e7b71a3da02d11f7c94007a40ca733267
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-23 22:25:50 +0200

    updated reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    len = word.length;
+    revWord = "";
+    for (i=len; i>0; i--) {
+        revWord += word.charAt(i-1);
+    }
+    return revWord;
 };
 
 // Do not edit below this line
