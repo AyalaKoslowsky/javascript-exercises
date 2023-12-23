89d2d392f9f05668c832a7a1669ec29424d97344
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-23 23:43:31 +0200

    updated leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap years are years divisible by four.
+    if (year % 4 != 0) {
+        return false;
+    }
 
+    // years divisible by 100 are not leap years unless they are divisible by 400.
+    else if (year % 100 == 0) { //divided by 4, 100
+        if (year % 400 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else { // divided by 4 and not by 100
+        return true;
+    }
 };
 
 // Do not edit below this line
