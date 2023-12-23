a921f745f30d6e31caffe579ec041f2fc68d2d33
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-24 00:26:25 +0200

    solved calculators

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let res = 0;
+  for (i=0; i<arr.length; i++) {
+    res += arr[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  res = 1;
+  for (i=0; i<arr.length; i++) {
+    res *= arr[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	res = 1;
+  for (i=1; i<=num; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
