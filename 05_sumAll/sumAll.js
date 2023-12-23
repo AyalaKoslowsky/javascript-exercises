8cf8473ad6e9fa331fb514a138a448e838e0cdfc
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-23 23:33:21 +0200

    updated sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum = 0;
 
+    // returns ERROR with non-number parameters
+    if (typeof(first) != 'number' || typeof(last) != 'number') {
+        return 'ERROR';
+    }
+    // returns ERROR with negative numbers
+    if (first < 0 || last < 0) {
+        return 'ERROR';
+    }
+    // works with larger number first
+    if (last < first) {
+        let tmp = first;
+        first = last;
+        last = tmp;
+    }
+
+    for (i=first; i<=last; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
