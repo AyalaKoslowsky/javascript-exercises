e42d63fcb3c2d594ba1005085a73579d2e4b9d12
Ayala Koslowsky <ko.ayala@gmail.com>
2023-12-23 23:57:56 +0200

    updated tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let converted = (degree - 32) * 5 / 9;
+  let rounded = parseFloat(converted.toFixed(1));
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let converted = (degree * 9 / 5) + 32;
+  let rounded = parseFloat(converted.toFixed(1));
+  return rounded;
 };
 
 // Do not edit below this line
